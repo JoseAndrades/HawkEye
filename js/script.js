d330dc596c47ee609f9283b8417df8c095520d97
JoseAndrades <josemanuel.andrades.alu@iescampanillas.com>
2022-02-19 09:56:43 +0100

    inserta Mensaje aquí

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//carrousel
+$('.carousel-inner').carousel({
+  interval: 2000,
+  pause: false,
+  bucle: true,
+  wrap: false
+});
+
+
+
+function redireccionar() 
+{
+  location.href="../form.html";
+};
